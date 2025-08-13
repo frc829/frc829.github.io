322c0fd99af4d974763d73e17cb3a8ab4cd74e32
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-08-13 13:54:23 -0400

    extracted styles from responsibilities

--- programming/responsibilities/src/App.jsx
+++ programming/responsibilities/src/App.jsx
@@ -1,12 +1,15 @@
-import css from './styles/App.css'
+import './styles/App.css'
 
 function App() {
 
-  return (
-    <>
-
-    </>
-  )
+    return (
+        <div className="wrap">
+            <h1>FRC Programming Team Org Chart</h1>
+            <p className="lead">Configuration: <span className="pill">1 Programming Mentor</span> + <span
+                className="pill">4 Students</span>
+            </p>
+        </div>
+    )
 }
 
 export default App
